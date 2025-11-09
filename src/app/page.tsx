e058ded3eca26f94230d36ba25f37820610f47a9
afganirw07 <afganirwansyah070708@gmail.com>
2025-11-10 00:09:19 +0700

    feat: add hero section

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,12 @@
 import React from "react";
 import { Navbar } from "@/components/landing/navbar";
-
+import HeroSection from "@/components/landing/HeroSection/hero";
 export default function Homepage() {
 
     return(
         <div>
             <Navbar />
+            <HeroSection />
         </div>
     );
 }
---
+++ src/components/landing/HeroSection/hero.tsx
@@ -0,0 +1,50 @@
+import Badge from "@/components/ui/badge/Badge";
+import { Button } from "@/components/ui/button/Navbutton";
+import { ArrowUpRight, Info } from "lucide-react";
+import Image from "next/image";
+
+const HeroSection = () => {
+    return (
+        <div className="min-h-[calc(100vh-4rem)] w-full flex items-center justify-center overflow-hidden border-b border-accent">
+            <div className="max-w-(--breakpoint-xl) w-full flex flex-col lg:flex-row mx-auto items-center justify-between gap-y-14 gap-x-10 px-6 py-12 lg:py-0">
+                <div className="max-w-xl">
+                    <Badge className="rounded-full py-1 border-none">
+                        Just released v1.0.0
+                    </Badge>
+                    <h1 className="mt-6 max-w-[20ch] text-3xl xs:text-4xl sm:text-5xl lg:text-[2.75rem] xl:text-5xl font-bold leading-[1.2]! tracking-tight">
+                        Jelajahi Ilmu, Berkreasi, dan Berkarya di Pusat Literasi Digital Nasional
+                    </h1>
+                    <p className="mt-6 max-w-[60ch] xs:text-lg">
+                        Akses ribuan koleksi buku, e-book, dan ruang literasi modern.
+                        Pinjam, baca, dan temukan komunitas pembaca dari seluruh Indonesia.
+                    </p>
+                    <div className="mt-12 flex flex-col sm:flex-row items-center gap-4">
+                        <Button
+                            size="lg"
+                            className="w-full sm:w-auto rounded-full text-base"
+                        >
+                            Cari & Pesan Buku <ArrowUpRight className="h-5! w-5!" />
+                        </Button>
+                        <Button
+                            variant="outline"
+                            size="lg"
+                            className="w-full sm:w-auto rounded-full text-base shadow-none"
+                        >
+                            <Info className="h-5! w-5!" /> Panduan Pinjam Online
+                        </Button>
+                    </div>
+                </div>
+                <div className="relative lg:max-w-lg xl:max-w-xl w-full bg-accent rounded-xl aspect-square">
+                    <Image
+                        src="/images/landing/hero.png"
+                        fill
+                        alt=""
+                        className="object-cover rounded-xl"
+                    />
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default HeroSection;
