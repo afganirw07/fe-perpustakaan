abd8b30288454b3c57abcdf69c5c56c7eb4795fa
afganirw07 <afganirwansyah070708@gmail.com>
2025-11-11 11:39:07 +0700

    fix: loyout.tsx light mode

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -49,55 +49,30 @@ export const metadata: Metadata = {
   ],
   creator: "Akash Moradiya",
   icons: [
-    {
-      rel: "icon",
-      url: "/favicon.ico",
-    },
-    {
-      rel: "apple-touch-icon",
-      url: "/apple-touch-icon.png",
-    },
-    {
-      rel: "icon",
-      type: "image/png",
-      url: "/favicon-32x32.png",
-      sizes: "32x32",
-    },
-    {
-      rel: "icon",
-      type: "image/png",
-      url: "/favicon-16x16.png",
-      sizes: "16x16",
-    },
-    {
-      rel: "icon",
-      type: "image/png",
-      url: "/android-chrome-192x192.png",
-      sizes: "192x192",
-    },
-    {
-      rel: "icon",
-      type: "image/png",
-      url: "/android-chrome-512x512.png",
-      sizes: "512x512",
-    },
+    { rel: "icon", url: "/favicon.ico" },
+    { rel: "apple-touch-icon", url: "/apple-touch-icon.png" },
+    { rel: "icon", type: "image/png", url: "/favicon-32x32.png", sizes: "32x32" },
+    { rel: "icon", type: "image/png", url: "/favicon-16x16.png", sizes: "16x16" },
+    { rel: "icon", type: "image/png", url: "/android-chrome-192x192.png", sizes: "192x192" },
+    { rel: "icon", type: "image/png", url: "/android-chrome-512x512.png", sizes: "512x512" },
   ],
-  robots: {
-    index: true,
-    follow: true,
-  },
+  robots: { index: true, follow: true },
   manifest: "/site.webmanifest",
 };
 
 export default function RootLayout({
   children,
-}: Readonly<{
+}: {
   children: React.ReactNode;
-}>) {
+}) {
   return (
     <html lang="en" suppressHydrationWarning>
       <body className={`${geistSans.className} antialiased`}>
-        <ThemeProvider attribute="class" defaultTheme="system" enableSystem>
+        <ThemeProvider
+          attribute="class"
+          defaultTheme="light"    
+          enableSystem={false}    
+        >
           {children}
         </ThemeProvider>
       </body>
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,16 +1,14 @@
 import type { Config } from "tailwindcss";
 
 const config: Config = {
-    darkMode: false,
+    darkMode: ["class"],
     content: [
         "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
         "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
         "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
         "./src/layout/**/*.{js,ts,jsx,tsx,mdx}",
     ],
-    theme: {
-        extend: {},
-    },
+    theme: { extend: {} },
     plugins: [
         require("@tailwindcss/forms"),
         require("tailwindcss-animate"),
@@ -18,3 +16,4 @@ const config: Config = {
 };
 
 export default config;
+
